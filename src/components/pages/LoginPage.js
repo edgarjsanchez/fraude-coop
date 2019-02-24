aca96ef37c3d2aeb1089f5ae902cc70b541588c4
edgarjsanchez <edgarjsanchez@gmail.com>
2019-02-24 14:51:38 -0500

    Token en corpID var

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -2,15 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import LoginForm from "../forms/LoginForm";
 import { AsyncStorage, StyleSheet } from "react-native";
-import { login, test } from "../../api/auth";
+import { login } from "../../api/auth";
 import setAuthorization from "../../utils/setAuthorizationHeader";
 import * as Keychain from "react-native-keychain";
+import { corpID } from "../../utils/environment";
 
 class LoginPage extends React.Component {
-  componentWillMount() {
-    setAuthorization(
-      "eyJhbGciOiJIUzI1NiJ9.eyJjb21wYW5pYSI6Ik15U29mdHdhcmVPViJ9.mr6os1iR-3ywUgDaTGQXqHPfIDHdzhI8YAAbEZlGhzU"
-    );
+  componentDidMount() {
+    setAuthorization(corpID.token);
   }
 
   submit = data =>
--- src/utils/environment.js
+++ src/utils/environment.js
@@ -9,6 +9,11 @@ export const URL = {
   reset_password: "http://192.168.89.130:8888/api/reset_password"
 };
 
+export const corpID = {
+  token:
+    "eyJhbGciOiJIUzI1NiJ9.eyJjb21wYW5pYSI6Ik15U29mdHdhcmVPViJ9.mr6os1iR-3ywUgDaTGQXqHPfIDHdzhI8YAAbEZlGhzU"
+};
+
 /* export const URL = {
   auth: "http://192.168.86.219:8888/api/auth",
   viajes: "http://192.168.86.219:8888/api/viajes",
